915d8cf1f009a7e238b8043bed43961a9fae0b62
sininen-blue <alaanneil@gmail.com>
2025-02-14 12:47:15 +0800

    feat: added factories for coures and department

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model
 {
-    //
+    use HasFactory;
+    // TODO: figure out the whole belongs-to-has thing
 }
---
+++ database/factories/CourseFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Course>
+ */
+class CourseFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'department_id' => 1, // TODO: fix
+            'code' => 'bscs',
+            'acronym' => 'cs',
+            'description' => 'bachelor of science in computer science',
+            'major' => 'major in data science',
+            'year_granted' => '2004',
+            'years' => 4,
+        ];
+    }
+}
