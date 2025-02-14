9da35aa212e698f6a0a72e0f55bb6d3ad71fb91a
sininen-blue <alaanneil@gmail.com>
2025-02-14 13:38:02 +0800

    fix(models): added relationship for course/department

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Course extends Model
 {
     use HasFactory;
     // TODO: figure out the whole belongs-to-has thing
+    //
+    public function department(): BelongsTo
+    {
+        return $this->belongsTo(Department::class);
+    }
 }
