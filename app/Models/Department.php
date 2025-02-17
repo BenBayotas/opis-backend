5cfb6713595c464ce67b508305c04f2cf307aeb7
sininen-blue <alaanneil@gmail.com>
2025-02-17 14:18:04 +0800

    feat: added the belongs to classes to dep and sub

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Department extends Model
@@ -19,5 +20,11 @@ class Department extends Model
     public function program_type(): BelongsTo
     {
         return $this->belongsTo(ProgramType::class);
+    }
+
+
+    public function subjects(): BelongsToMany
+    {
+        return $this->belongsToMany(Subject::class);
     }
 }
--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Subject extends Model
 {
     /** @use HasFactory<\Database\Factories\SubjectFactory> */
     use HasFactory;
+
+    public function departments(): BelongsToMany
+    {
+        return $this->belongsToMany(Department::class);
+    }
 }
