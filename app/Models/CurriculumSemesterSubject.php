bbabf398c9ada5ed86b5e244985b4f8d24175021
sininen-blue <alaanneil@gmail.com>
2025-02-17 16:19:47 +0800

    faat: added curriculum semester subject pivot map

---
+++ app/Models/CurriculumSemesterSubject.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CurriculumSemesterSubject extends Model
+{
+    /** @use HasFactory<\Database\Factories\CurriculumSemesterSubjectFactory> */
+    use HasFactory;
+}
