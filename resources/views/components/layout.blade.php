4b11d7fa312583f45690a19068a210ad4130dc7d
BenBayotas <elijahben.bayotas@gmail.com>
2025-02-18 13:11:30 +0800

    resolved  convos
    -deleted testcontroller
    -deleted deptseeder
    -for htmx and json, keep it as it is, but wont be implemented in the future

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,7 +7,7 @@
         
         <title>{{ $title }}</title>
     </head>
-    <body>
+    <body class="container">
         <script src="https://unpkg.com/htmx.org@1.9.2"></script>
         {{ $slot }}
     </body>
