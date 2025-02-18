46a9157d7efb24e61be3e93cb535325f36f1f68a
sininen-blue <alaanneil@gmail.com>
2025-02-18 11:48:54 +0800

    feat: added relationship to cur and curYear

--- app/Models/Curriculum.php
+++ app/Models/Curriculum.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Curriculum extends Model
 {
     /** @use HasFactory<\Database\Factories\CurriculumFactory> */
     use HasFactory;
+
+    public function curriculumYear(): HasMany
+    {
+        return $this->hasMany(CurriculumYear::class);
+    }
 }
--- app/Models/CurriculumYear.php
+++ app/Models/CurriculumYear.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class CurriculumYear extends Model
 {
     /** @use HasFactory<\Database\Factories\CurriculumYearFactory> */
     use HasFactory;
+
+    public function curriculum(): BelongsTo
+    {
+        return $this->belongsTo(Curriculum::class);
+    }
 }
