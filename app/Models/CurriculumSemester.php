dc3f5d72f89563282e95889f0cea84d086356126
sininen-blue <alaanneil@gmail.com>
2025-02-18 11:51:40 +0800

    feat: adde relation to year and sem

--- app/Models/CurriculumSemester.php
+++ app/Models/CurriculumSemester.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class CurriculumSemester extends Model
 {
     use HasFactory;
+
+    public function curriculumYear(): BelongsTo
+    {
+        return $this->belongsTo(CurriculumYear::class);
+    }
 }
--- app/Models/CurriculumYear.php
+++ app/Models/CurriculumYear.php
@@ -5,11 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class CurriculumYear extends Model
 {
     /** @use HasFactory<\Database\Factories\CurriculumYearFactory> */
     use HasFactory;
+
+    public function semesters(): HasMany
+    {
+        return $this->hasMany(CurriculumSemester::class);
+    }
+
 
     public function curriculum(): BelongsTo
     {
