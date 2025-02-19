77c8bdf4d1ed47ae2274c53608a1473769c4f5de
sininen-blue <alaanneil@gmail.com>
2025-02-19 12:35:14 +0800

    fir: added course curriculum relationship

--- app/Models/Curriculum.php
+++ app/Models/Curriculum.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Curriculum extends Model
@@ -15,5 +16,10 @@ class Curriculum extends Model
     {
         return $this->hasMany(CurriculumYear::class)
             ->orderBy('year', 'asc');
+    }
+
+    public function course(): BelongsTo
+    {
+        return $this->belongsTo(Course::class);
     }
 }
