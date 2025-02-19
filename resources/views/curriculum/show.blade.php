31359f9548539bfd14f3420d477091e7e8d1c77e
BenBayotas <elijahben.bayotas@gmail.com>
2025-02-19 15:41:23 +0800

    made changes to models

--- resources/views/curriculum/show.blade.php
+++ resources/views/curriculum/show.blade.php
@@ -6,14 +6,87 @@
 
     <h1>Curriculum {{ $curriculum->curriculum_year }} of {{ $curriculum->course->description }}</h1>
 
-    <a role="button" href="{{ route('curriculum.edit', $curriculum) }}">Edit</a>
+     {{-- <a role="button" href="{{ route('curriculum.edit', $curriculum) }}">Edit</a> --}}
+    
+    <div class="grid">
+        <label>
+            Curriculum Year
+            <select name="curriculum_year" id="curriculum_year">
+                    <option></option>
+            </select>
+        </label>
+
+        <label>
+            Course
+            <select name="course" id="course">
+                <option></option>
+            </select>
+        </label>
+    </div>
+
+    <h3>Add Curriculum Subjects</h3>
+
+    <div class="grid">
+        <label>
+            Year
+            <input type="text">
+        </label>
+
+        <label>
+            Semester
+            <select name="semester" id="semester">
+                <option></option>
+            </select>
+        </label>
+    </div>
+
+    <div class="grid">
+        <label>
+            All Subjects
+            <select name="subject_major" id="subject_major">
+                <option></option>
+            </select>
+        </label>
+
+        <label>
+            Search Subjects
+            <input type="search">
+        </label>
+
+        <label>
+            Filter Selected Subjects
+            <input type="checkbox">
+        </label>
+    </div>
+    
+    <div class="grid">
+        <label>
+            All Available Subjects 
+        
+        </label>
+
+        <label>
+            Filter Selected Subjects
+            <input type="checkbox">
+        </label>
+
+        <label>
+            Selected Subjects 
+        </label>
+    </div>
+
+    <div>
+        <label>
+            <button>Add Subjects</button>
+        </label>
+    </div>
 
     @foreach ($curriculum->curriculumYears as $year)
     <div>
-        <h2>Year {{ $year->year }}</h2>
+        <h4>Year {{ $year->year }}</h4>
 
         @foreach ($year->semesters as $sem)
-        <h3>{{ $sem->title }}</h3>
+        <h5>{{ $sem->title }}</h5>
         <table>
             <thead>
                 <th scope="col">Subject Code</th>
