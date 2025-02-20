f10f79afdba9cb113582ce71f43675e34a076fd1
sininen-blue <alaanneil@gmail.com>
2025-02-20 11:21:29 +0800

    feat: lower font size, nav bar

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,10 +4,30 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
-        
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.colors.min.css">
         <title>{{ $title }}</title>
     </head>
     <body class="container">
+        <style>
+            :root {
+                --pico-font-size: 90%;
+            }
+        </style>
+
+
+        <header>
+            <nav>
+                <ul>
+                    <li><strong>Opis Mock Backend</strong></li>
+                </ul>
+                <ul>
+                    <li><a href="{{ route('course.index') }}">Course</a></li>
+                    <li><a href="{{ route('subject.index') }}">Subject</a></li>
+                    <li><a href="{{ route('curriculum.index') }}">Curriculum</a></li>
+                </ul>
+            </nav>
+        </header>
+
         <script src="https://unpkg.com/htmx.org@1.9.2"></script>
         {{ $slot }}
     </body>
