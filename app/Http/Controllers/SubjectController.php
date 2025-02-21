45ba96bacff92203565c2565a762e47a65bae2d4
sininen-blue <alaanneil@gmail.com>
2025-02-21 15:28:34 +0800

    fix: removed is_major and added todo

--- app/Http/Controllers/SubjectController.php
+++ app/Http/Controllers/SubjectController.php
@@ -16,7 +16,6 @@ class SubjectController extends Controller
     {
         $subjects = Subject::with('department')->get();
         return view('subject.subject-index', compact('subjects'));
-
     }
 
     /**
@@ -25,11 +24,10 @@ class SubjectController extends Controller
     public function create()
     {
         $departments = Department::all();
-        $subjectGroups =DB::table('subject_groups')->get();
+        $subjectGroups = DB::table('subject_groups')->get();
         $subjectCategories = DB::table('subject_categories')->get();
 
-        return view('subject.subject-create', compact('departments','subjectGroups','subjectCategories'));
-
+        return view('subject.subject-create', compact('departments', 'subjectGroups', 'subjectCategories'));
     }
 
     /**
@@ -41,7 +39,6 @@ class SubjectController extends Controller
             'subject_group_id' => 'required|exists:subject_groups,id',
             'code'     => 'nullable|string',
             'title'    => 'required|string',
-            'is_major'         => 'required|boolean',
             'department_id'    => 'required|exists:departments,id',
             'credited_units'   => 'required|integer',
             'lec_hours'        => 'required|integer',
@@ -89,7 +86,6 @@ class SubjectController extends Controller
             'subject_group_id' => 'required|exists:subject_groups,id',
             'code'     => 'nullable|string',
             'title'    => 'required|string',
-            'is_major'         => 'required|boolean',
             'department_id'    => 'required|exists:departments,id',
             'credited_units'   => 'required|integer',
             'lec_hours'        => 'required|integer',
@@ -118,16 +114,17 @@ class SubjectController extends Controller
 
     public function manageRequisites(Request $request, $subjectId)
     {
-       
+
         $curriculumId = $request->query('curriculum');
         $subject = Subject::findOrFail($subjectId);
         $curriculum = \App\Models\Curriculum::findOrFail($curriculumId);
-        $allSubjects = Subject::all(); 
-        
+        $allSubjects = Subject::all();
+
         return view('subject.manage-requisites', compact('subject', 'curriculum', 'allSubjects'));
     }
 
-    public function storeRequisites(Request $request, Subject $subject){
+    public function storeRequisites(Request $request, Subject $subject)
+    {
         $data = $request->validate([
             'subject_id'    => 'required|exists:subjects,id',
             'curriculum_id' => 'required|exists:curricula,id',
@@ -138,15 +135,15 @@ class SubjectController extends Controller
             'equivalents'   => 'nullable|array',
             'equivalents.*' => 'exists:subjects,id',
         ]);
-    
+
         $subject = Subject::findOrFail($data['subject_id']);
         $curriculumId = $data['curriculum_id'];
-    
+
         // Detach existing relationships for this curriculum
         $subject->prerequisites()->wherePivot('curriculum_id', $curriculumId)->detach();
         $subject->corequisites()->wherePivot('curriculum_id', $curriculumId)->detach();
         $subject->equivalents()->wherePivot('curriculum_id', $curriculumId)->detach();
-    
+
         // Attach the new prerequisites (if any)
         if (!empty($data['prerequisites'])) {
             $prereqData = array_fill_keys($data['prerequisites'], ['curriculum_id' => $curriculumId]);
@@ -162,7 +159,7 @@ class SubjectController extends Controller
             $equivData = array_fill_keys($data['equivalents'], ['curriculum_id' => $curriculumId]);
             $subject->equivalents()->syncWithoutDetaching($equivData);
         }
-    
+
         return redirect()->route('curriculum.show', ['curriculum' => $curriculumId])
             ->with('success', 'Requisites updated successfully.');
     }
