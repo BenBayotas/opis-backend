96ad192a8100db52fbb8cd54d98b03f69b3bb67e
sininen-blue <alaanneil@gmail.com>
2025-02-21 14:50:31 +0800

    feat curriculum

--- app/Models/Curriculum.php
+++ app/Models/Curriculum.php
@@ -2,10 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Curriculum extends Model
 {
@@ -20,5 +19,13 @@ class Curriculum extends Model
     public function department(): BelongsTo
     {
         return $this->belongsTo(Department::class);
+    }
+
+    public function subjects(): BelongsToMany
+    {
+        return $this->belongsToMany(Subject::class, 'curriculum_subject')
+            ->using(CurriculumSubject::class)
+            ->withPivot('year_level', 'semester', 'quota', 'subject_area_id')
+            ->withTimestamps();
     }
 }
